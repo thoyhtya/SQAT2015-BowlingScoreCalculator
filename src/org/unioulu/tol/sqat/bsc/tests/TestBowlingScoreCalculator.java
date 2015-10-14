1b34527e054102462393be7d57cc81895a98207d
somename <someemail>
2015-10-14 14:53:48 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -24,11 +24,20 @@ public class TestBowlingScoreCalculator {
 		Frame frame = new Frame(0,9);
 		assertEquals(9, frame.score());
 	}
-	
+		
 	@Test
 	public void testGameInit() {
 		BowlingGame game = new BowlingGame();
 		game.addFrame(new Frame(1,5));
+		game.addFrame(new Frame(3,6));
+		game.addFrame(new Frame(7,2));
+		game.addFrame(new Frame(3,6));
+		game.addFrame(new Frame(4,4));
+		game.addFrame(new Frame(5,3));
+		game.addFrame(new Frame(3,3));
+		game.addFrame(new Frame(4,5));
+		game.addFrame(new Frame(8,1));
+		game.addFrame(new Frame(2,6));
 	}
 
 }
