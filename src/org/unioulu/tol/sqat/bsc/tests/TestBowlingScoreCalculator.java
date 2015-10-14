c04e7b6ecafc66b7a09f33fcd33ec0ed578ea442
somename <someemail>
2015-10-14 14:45:33 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -3,12 +3,19 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.Frame;
 
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void test() {
+	public void testFrameInit() {
 		Frame frame = new Frame(2,4);
+	}
+	
+	@Test
+	public void testFrameScore_2_6_is_8() {
+		Frame frame = new Frame(2,6);
+		assertEquals(8, frame.score());
 	}
 
 }
